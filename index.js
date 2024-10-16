e8b6c8d0192b7c31a72002c90d5d9fa9e2bc13cc
Rakesh Rout <rr908654@gmail.com>
2024-10-16 11:48:30 +0530

    negative number handling

--- index.js
+++ index.js
@@ -9,16 +9,31 @@ function add(number) {
         number = parts.slice(1).join('\n');
     }
 
+    const negatives = [];
     const numList = number.split(new RegExp(`[${delimiter}\\n]`));
     let total = 0;
 
     for (let num of numList) {
-        total += parseInt(num) || 0;
+        const parsedNum = parseInt(num);
+        if (parsedNum < 0) {
+            negatives.push(parsedNum);
+        }
+        total += parsedNum || 0;
+    }
+
+    if (negatives.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negatives.join(', ')}`);
     }
 
     return total;
 }
 
 // const res = add("1,2\n3,4\n5,6,7\n8");
-const res = add("//*\n1*2*3*4");
-console.log(res);
+// const res = add("//*\n1*2*3*4");
+// console.log(res);
+try {
+    // console.log(add("-1,2,-3"));
+    console.log(add("//|\n1|2|3|-4"));
+} catch (error) {
+    console.error(error.message);
+}
