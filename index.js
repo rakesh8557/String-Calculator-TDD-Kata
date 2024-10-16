71a126889de0e294110c3fc69a752aa4b1ad120e
Rakesh Rout <rr908654@gmail.com>
2024-10-16 11:16:49 +0530

    comma separted string numbers addition

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function add(number) {
     if(!number)
         return 0;
+    const numList = number.split(',');
+    let total = 0;
 
+    for (let num of numList) {
+        total += parseInt(num) || 0;
+    }
+
+    return total;
 }
 
-const res = add("");
+const res = add("1,2,3,4");
 console.log(res);
